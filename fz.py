7e37c137d7529d20ac172f1c4d2bee1db21a4d49
Serena Fang <Serena_Fang@hms.harvard.edu>
2023-03-07 01:35:05 -0500

    test

--- fz.py
+++ fz.py
@@ -2,11 +2,16 @@ import altair as alt
 import pandas as pd
 import streamlit as st
 
-def load_data():
-    df = pd.read_csv("https://raw.githubusercontent.com/Jinrui93/706Final/main/data.csv?token=GHSAT0AAAAAAB6HXXFMX337YJ7QKB7FWQHSZAG3FJA")
+df = pd.read_csv('data.csv')
 
-    return df
+df['date'] = pd.to_datetime(df['date'])
+df['year_month'] = df['date'].dt.to_period('M')
+df['year_month'] = df['year_month'].astype(str).str.replace('-', '').astype(int)
+df['day'] = df['date'].dt.day
+df['day'] = df['day'].astype(int)
 
-df = load_data()
+default_ym = 202003
+ym_slider = st.slider('Year_Month', min(df['year_month']), max(df['year_month']), value=default_ym)
 
 st.write("## Association between Vaccination Rate and Number of Covid Cases")
+
