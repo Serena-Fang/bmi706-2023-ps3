3392ade355380b4a1023e083582c1968ce7228c1
Serena Fang <Serena_Fang@hms.harvard.edu>
2023-03-07 11:47:26 -0500

    t

--- fz.py
+++ fz.py
@@ -3,17 +3,20 @@ import pandas as pd
 import streamlit as st
 
 df = pd.read_csv('data.csv')
+df1 = pd.read_csv('data_month.csv')
 
-df['date'] = pd.to_datetime(df['date'])
-df['year_month'] = df['date'].dt.to_period('M')
-df['year_month'] = df['year_month'].astype(str).str.replace('-', '').astype(int)
-df['day'] = df['date'].dt.day
-df['day'] = df['day'].astype(int)
+# df['date'] = pd.to_datetime(df['date'])
+# df['year_month'] = df['date'].dt.to_period('M')
+# df['year_month'] = df['year_month'].astype(str).str.replace('-', '').astype(int)
+# df['day'] = df['date'].dt.day
+# df['day'] = df['day'].astype(int)
 
 default_ym = 202003
-ym_slider = st.slider('Year_Month', min(df['year_month']), max(df['year_month']), value=default_ym)
+ym_slider = st.slider('Year_Month', min(df1['new_date']), max(df1['new_date']), value=default_ym)
 
 st.write("## Association between Vaccination Rate and Number of Covid Cases")
 
-state_selectbox = st.selectbox(
-    "State", tuple(df["state"].unique()))
+# state_filtered = df[df['state'] == state_selectbox]
+
+df5 = df[(df['year_month'] == ym_slider)]
+
