97ef7cec50712d4c1cc1643bcfc7af5a2eb25509
Serena Fang <Serena_Fang@hms.harvard.edu>
2023-03-07 01:25:24 -0500

    test

--- fz.py
+++ fz.py
@@ -2,11 +2,11 @@ import altair as alt
 import pandas as pd
 import streamlit as st
 
-def load_data():
-    df = pd.read_csv("https://github.com/Jinrui93/706Final/blob/main/data.csv")
+# def load_data():
+#     df = pd.read_csv("https://github.com/Jinrui93/706Final/blob/main/data.csv")
 
-    return df
+#     return df
 
-df = load_data()
+# df = load_data()
 
 st.write("## Association between Vaccination Rate and Number of Covid Cases")
